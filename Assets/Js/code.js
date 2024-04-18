92b2a0292a060d12fb1a573b6c6b922525aba15f
JesarRL <ljesar01@gmail.com>
2024-04-18 18:41:49 -0500

    subiendo avances

--- Assets/Js/code.js
+++ Assets/Js/code.js
@@ -46,7 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
 let Gasolina_ford_f150 ={
     nombre: "Ford F-150",
     combustible: "Gasolina",
-    cilindraje: "3,500 c.c",
     potencia: "290 hp",
     torque: "359.2 Nm",
     medidas: "1,960 mm (alto) 2,030 mm (ancho) 5,890 mm (largo)",
@@ -56,7 +55,6 @@ let Gasolina_ford_f150 ={
 let Gasolina_toyota_tundra ={
     nombre: "Toyota Tundra",
     combustible: "Gasolina",
-    cilindraje: "5,663 c.c",
     potencia: "375.81 hp",
     torque: "544 Nm",
     medidas: "Medidas: 1,935 mm (Alto) 2029 mm (Ancho) 5,814 mm (Largo)",
@@ -66,7 +64,6 @@ let Gasolina_toyota_tundra ={
 let Gasolina_nissan_frontier ={
     nombre: "Nissan Frontier",
     combustible: "Gasolina",
-    cilindraje: "2,500 c.c",
     potencia: "158 hp",
     torque: "233 Nm",
     medidas: "1,803 mm (Altura) 1,850 (Ancho) 5,253 (Laargo)",
@@ -76,7 +73,6 @@ let Gasolina_nissan_frontier ={
 let Gasolina_dodge_ram ={
     nombre: "Dodge Ram 1500",
     combustible: "Gasolina",
-    cilindraje: "5,654 c.c",
     potencia: "395 hp",
     torque: "548 Nm",
     medidas: "1,981 mm (alto) 2,093 mm (ancho) 6,060 mm (Largo)",
@@ -86,7 +82,6 @@ let Gasolina_dodge_ram ={
 let Diesel_ford_f450 ={
     nombre: "Ford F-450 ",
     combustible: "Diesel",
-    cilindraje: "6,700 c.c",
     potencia: "330 hp",
     torque: "1,288.03 Nm",
     medidas: "2,085 mm (Alto) 2,438 mm (Ancho) 6,751 mm (Largo)",
@@ -96,7 +91,6 @@ let Diesel_ford_f450 ={
 let Diesel_mitsubishi_l200 ={
     nombre: "Mitsubishi L200",
     combustible: "Diesel",
-    cilindraje: "2,442 C.C",
     potencia: "136 hp",
     torque: "430 Nm",
     medidas: "1.815 mm (ancho)  1.795 mm (alto) 5.220 mm (largo)",
@@ -106,7 +100,6 @@ let Diesel_mitsubishi_l200 ={
 let Diesel_dodge_ram2500 ={
     nombre: "Dodge Ram 2500 Laramie",
     combustible: "Diesel",
-    cilindraje: "6,708 c.c",
     potencia: "370 hp",
     torque: "1,152.45 Nm",
     medidas: "1,981 mm (alto) 2,093 mm (ancho) 6,060 mm (Largo)",
@@ -116,7 +109,6 @@ let Diesel_dodge_ram2500 ={
 let Diesel_toyota_hilux ={
     nombre: "Toyota Hilux",
     combustible: "Diesel",
-    cilindraje: "2,700 c.c",
     potencia: "166 hp",
     torque: "244.05 Nm",
     medidas: "1,815 mm (alto) 1,815 mm (ancho) 5,335 mm (Largo)",
@@ -126,7 +118,6 @@ let Diesel_toyota_hilux ={
 let Electrica_ford_f150 ={
     nombre: "Ford F-150 Lightning",
     combustible: "Electrica",
-    capacidad_bateria: "131 kWh",
     potencia: "562,24 Hp",
     torque: "1,050 Nm",
     medidas: "2,000 mm (alto) 2,030 mm (ancho) 5,890 mm (largo)",
@@ -136,7 +127,6 @@ let Electrica_ford_f150 ={
 let Electrica_jac_t8 ={
     nombre: "Jac T8 ",
     combustible: "Electrica",
-    capacidad_bateria: "65,3 kWh",
     potencia: "140 hp",
     torque: "340 Nm",
     medidas: "1,830 mm (alto) 1,880 mm (ancho) 5,325 mm (largo)",
@@ -146,7 +136,6 @@ let Electrica_jac_t8 ={
 let Electrica_radar_rd6 ={
     nombre: "Radar RD6 EV",
     combustible: "Electrica",
-    capacidad_bateria: "100 kWh",
     potencia: "268 hp",
     torque: "384 Nm",
     medidas: "1,865 mm (alto) 1,900 mm (ancho) 5,260 mm (largo)",
@@ -156,7 +145,6 @@ let Electrica_radar_rd6 ={
 let Electrica_dongfeng_rich ={
     nombre: "Dongfeng Rich",
     combustible: "Electrica",
-    capacidad_bateria: "67.09 kWh",
     potencia: "161 Hp",
     torque: "420 Nm",
     medidas: "1,820 mm (alto) 1,850 mm (ancho) 5,290 mm (largo)",
